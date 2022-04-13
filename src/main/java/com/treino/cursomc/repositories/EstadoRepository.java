1de2e4d7af555f9d20dda7b71aec2c496de00e0b
Daniel Moraes <daniel_fery@hotmail.com>
2022-04-13 11:50:34 -0300

    Estado e Cidade

---
+++ src/main/java/com/treino/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.treino.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.treino.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
