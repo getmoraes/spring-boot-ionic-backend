4521aac08a5795cc73838ae7b3e893e7959326a0
Daniel Moraes <daniel_fery@hotmail.com>
2022-05-03 13:41:20 -0300

    Correcoes: update em CategoriaService e construtor de Pagamento

--- src/main/java/com/treino/cursomc/domain/Categoria.java
+++ src/main/java/com/treino/cursomc/domain/Categoria.java
@@ -43,11 +43,11 @@ public class Categoria implements Serializable {
 		this.id = id;
 	}
 
-	public String getName() {
+	public String getNome() {
 		return nome;
 	}
 
-	public void setName(String name) {
+	public void setNome(String name) {
 		this.nome = name;
 	}
 
